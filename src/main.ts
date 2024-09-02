41f035152f2ecbddc010758d45bd0a579dd8b4a2
Samatara <samatarbulbula@gmail.com>
2024-09-02 20:05:54 +0200

    start
    starta hele oppgaven på nytt men på typscript og hentet html fra js versjonen som ikke helt funket

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import './style.css'
+
+const form= document.getElementById("project-form");
+const projectlist= document.getElementById("pro_list");
+const projectarray=[];
+
+form?.addEventListener("submit", async(event: SubmitEvent){
+  event.preventDefault();
+})
